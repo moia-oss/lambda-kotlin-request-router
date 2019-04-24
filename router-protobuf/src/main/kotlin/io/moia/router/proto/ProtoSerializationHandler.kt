2fa04703ecc1c1b4cc60c6c5f80f440a00b4aeb8
Yannick Block <yannick.block@moia.io>
2019-04-24 11:10:52 +0200

    Use Apache ContentType instead of Guava MimeType in ProtoSerializationHandler

--- router-protobuf/src/main/kotlin/io/moia/router/proto/ProtoSerializationHandler.kt
+++ router-protobuf/src/main/kotlin/io/moia/router/proto/ProtoSerializationHandler.kt
@@ -1,21 +1,21 @@
 package io.moia.router.proto
 
+import com.google.protobuf.GeneratedMessageV3
 import io.moia.router.ResponseEntity
 import io.moia.router.SerializationHandler
-import com.google.common.net.MediaType
-import com.google.protobuf.GeneratedMessageV3
+import org.apache.http.entity.ContentType
 import java.util.Base64
 
 class ProtoSerializationHandler : SerializationHandler {
 
-    private val json = MediaType.parse("application/json")
+    private val json = ContentType.parse("application/json")
 
-    override fun supports(acceptHeader: MediaType, response: ResponseEntity<*>): Boolean =
+    override fun supports(acceptHeader: ContentType, response: ResponseEntity<*>): Boolean =
         response.body is GeneratedMessageV3
 
-    override fun serialize(acceptHeader: MediaType, response: ResponseEntity<*>): String {
+    override fun serialize(acceptHeader: ContentType, response: ResponseEntity<*>): String {
         val message = response.body as GeneratedMessageV3
-        return if (acceptHeader.`is`(json)) {
+        return if (json.mimeType == acceptHeader.mimeType) {
             ProtoBufUtils.toJsonWithoutWrappers(message)
         } else {
             Base64.getEncoder().encodeToString(message.toByteArray())
