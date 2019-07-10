cb13e7d9ebaa7f33aa2ef63be9dc6f52edcee6b1
Raoul Kaltenhaeuser <raoul.kaltenhaeuser@moia.io>
2019-07-10 10:17:34 +0200

    add ApiRequestContext to Request

--- router/src/main/kotlin/io/moia/router/Router.kt
+++ router/src/main/kotlin/io/moia/router/Router.kt
@@ -78,6 +78,7 @@ data class Request<I>(val apiRequest: APIGatewayProxyRequestEvent, val body: I, 
     val pathParameters by lazy { UriTemplate.from(pathPattern).extract(apiRequest.path) }
     val queryParameters: Map<String, String>? by lazy { apiRequest.queryStringParameters }
     val multiValueQueryStringParameters: Map<String, List<String>>? by lazy { apiRequest.multiValueQueryStringParameters }
+    val requestContext by lazy { apiRequest.requestContext }
     fun getPathParameter(name: String): String = pathParameters[name] ?: error("Could not find path parameter '$name")
     fun getQueryParameter(name: String): String? = queryParameters?.get(name)
     fun getMultiValueQueryStringParameter(name: String): List<String>? = multiValueQueryStringParameters?.get(name)
